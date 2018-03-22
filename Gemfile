58c4ee2a6dcb704d196e7ad9f6a3d35c50502f67
huutung06021992 <tungnhptit@gmail.com>
2018-03-22 10:47:43 +0700

    chap10

--- Gemfile
+++ Gemfile
@@ -15,10 +15,14 @@ gem "config", "~> 1.7"
 gem "jquery-rails", "~> 4.3", ">= 4.3.1"
 gem "rails-ujs", "~> 0.1.0"
 gem "kaminari", "~> 1.1", ">= 1.1.1"
+gem "kaminari-bootstrap", "~> 3.0", ">= 3.0.1"
 gem "coffee-rails", "~> 4.2"
 gem "turbolinks", "~> 5"
 gem "jbuilder", "~> 2.5"
 gem "bcrypt", "~> 3.1", ">= 3.1.11"
+gem "faker", "~> 1.8", ">= 1.8.7"
+gem "will_paginate", "~> 3.1", ">= 3.1.6"
+gem "will_paginate-bootstrap", "~> 1.0", ">= 1.0.1"
 
 group :development, :test do
   gem "byebug", platforms: [:mri, :mingw, :x64_mingw]
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,11 @@
 class UsersController < ApplicationController
+  before_action :logged_in_user, except: %i(new show create)
+  before_action :correct_user, only: %i(edit update)
+  before_action :admin_user, only: :destroy
+  before_action :find_user, except: %i(index new create)
 
   def index
-    @users = User.select(:id, :name).page(params[:page]).per Settings.page
+    @users = User.page params[:page]
   end
 
   def new
@@ -9,11 +13,6 @@ class UsersController < ApplicationController
   end
 
   def show
-    @user = User.find_by id: params[:id]
-    if @user.nil?
-      flash[:error] = t "no_find_user"
-      redirect_to users_url
-    end
   end
 
   def create
@@ -28,9 +27,61 @@ class UsersController < ApplicationController
     end
   end
 
+  def edit
+  end
+
+  def update
+    if @user.update_attributes user_params
+      flash[:success] = t "profile_update"
+      redirect_to @user
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @user.destroy
+    flash[:success] = t "user_delete"
+    redirect_to users_url
+  end
+
+  def verify_user
+    @user = User.find_by id: params[:id]
+    redirect_to home_url unless @user.current_user? current_user
+  end
+
+  def current_user? user
+    user=current_user
+  end
+
   private
 
-    def user_params
-      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  def user_params
+    params.require(:user).permit :name, :email, :password, :password_confirmation
+  end
+
+  def logged_in_user
+    unless logged_in?
+      store_location
+      flash[:danger] = t "login_please"
+      redirect_to login_url
     end
+  end
+
+  def correct_user
+    @user = User.find_by id: params[:id]
+    redirect_to home_url unless current_user? @user
+  end
+
+  def admin_user
+    redirect_to home_url unless current_user.admin?
+  end
+
+  def find_user
+    @user = User.find_by id: params[:id]
+    if @user.nil?
+      flash[:error] = t "no_find_user"
+      redirect_to users_url
+    end
+  end
 end
