28ce94612364e94d278f5241b87a2705544fa874
huutung06021992 <tungnhptit@gmail.com>
2018-03-22 10:47:43 +0700

    chap13

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -1,12 +1,13 @@
 class PasswordResetsController < ApplicationController
-
-  before_action :get_user, only: %i(edit update)
+  before_action :check_email, only: %i(edit update)
   before_action :valid_user, only: %i(edit update)
   before_action :check_expiration, only: %i(edit update)
 
+  def new
+  end
+
   def create
     @user = User.find_by email: params[:password_reset][:email].downcase
-
     if @user
       @user.create_reset_digest
       @user.send_password_reset_email
@@ -23,11 +24,11 @@ class PasswordResetsController < ApplicationController
 
   def update
     if params[:user][:password].empty?
-      @user.errors.add :password, t("no_empty")
+      @user.errors.add(:password, t("no_empty"))
       render :edit
-    elsif @user.update_attributes user_params
+    elsif @user.update_attributes(user_params)
       log_in @user
-      @user.update_attribute :reset_digest, nil
+      @user.update_attribute(:reset_digest, nil)
       flash[:success] = t "reset_suc_pass"
       redirect_to @user
     else
@@ -36,15 +37,15 @@ class PasswordResetsController < ApplicationController
   end
 
   private
-
   def user_params
-    params.require(:user).permit :password, :password_confirmation
+      params.require(:user).permit :password, :password_confirmation
   end
 
-  def get_user
-    @user = User.find_by email: params[:email]
+  def check_email
+    @user = User.find_by(email: params[:email])
     if @user.nil?
       flash[:info] = t "email_empty"
+      redirect_to root_url
     end
   end
 
---
+++ config/initializers/carrier_wave.rb
@@ -0,0 +1,10 @@
+if Rails.env.production?
+  CarrierWave.configure do |config|
+    config.fog_credentials = {
+      provider: "AWS",
+      aws_access_key_id: "S3_ACCESS_KEY",
+      aws_secret_access_key: "S3_SECRET_KEY"
+    }
+    config.fog_directory = "S3_BUCKET"
+  end
+end
