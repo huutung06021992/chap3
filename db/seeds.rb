4855ea225fc8564e0cdcdf98882891b4ae2f34c2
huutung06021992 <tungnhptit@gmail.com>
2018-03-22 10:47:43 +0700

    chap11

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,15 +1,19 @@
-User.create!(name:  "tung",
+User.create! name:  "tung",
   email: "tung414@gmail.com",
   password: "0978446292",
   password_confirmation: "0978446292",
-  admin: true)
+  admin: true,
+  activated: true,
+  activated_at: Time.zone.now
 
 99.times do |n|
   name  = Faker::Name.name
   email = "example-#{n+1}@railstutorial.org"
   password = "password"
-  User.create!(name:  name,
+  User.create! name:  name,
     email: email,
     password: password,
-    password_confirmation: password)
+    password_confirmation: password,
+    activated: true,
+    activated_at: Time.zone.now
 end
