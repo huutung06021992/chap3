c84bae945a9ff9cd3291de8202a1b08d0c50aa78
huutung06021992 <tungnhptit@gmail.com>
2018-03-22 10:47:43 +0700

    chap5

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+    # Returns the full title on a per-page basis.
+  def full_title page_title = ""
+    if page_title.empty?
+      I18n.t("sample")
+    else
+      page_title + " | " + I18n.t("sample")
+    end
+  end
+
 end
